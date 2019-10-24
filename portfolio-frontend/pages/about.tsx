7470d06f29faec04d47be4c8330941e20f4215f1
Brandon Feist <feist.brandon@gmail.com>
2019-10-24 00:00:46 -0700

    Added static about me image and made it responsive to width

--- portfolio-frontend/pages/about.tsx
+++ portfolio-frontend/pages/about.tsx
@@ -18,7 +18,8 @@ class About extends React.Component<AboutProps> {
           <div className="about-container">
             <img
               className="about-img"
-              src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/40199357_10217175120355267_281514696568209408_o.jpg?_nc_cat=103&_nc_oc=AQmYcAuUl0ETcUV6PjWtl5eiKs560HHDkwxHYkLTHRb29T19MeOpVCD1rQktVHcEzOzqc0VcGCPmmc2frUAwTazj&_nc_ht=scontent-sjc3-1.xx&oh=4caa957600e6cdbe6fd672d574830b72&oe=5E2D824E"
+              src="/static/images/presidio-picnic-medium.jpg"
+              srcSet="/static/images/presidio-picnic-large.jpg 500w, /static/images/presidio-picnic-medium.jpg 400w"
               alt="A photo of Brandon Feist."
             />
             <p className="about-container__paragraph">
